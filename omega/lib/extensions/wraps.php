94463f1c31f63b8a9d7eaa8fcd753ae0e940a88f
László Monda <laci@monda.hu>
2014-03-22 17:27:34 +0100

    Make the navbar static.  Hide the title of the index page.  Customize CSS a bit.

--- omega/lib/extensions/wraps.php
+++ omega/lib/extensions/wraps.php
@@ -8,8 +8,8 @@ function omega_wrap_close() {
   echo omega_apply_atomic( 'wrap_close', '</div>');
 }
 
-add_action('omega_header', 'omega_wrap_open', 7 );
-add_action('omega_header', 'omega_wrap_close' );
+//add_action('omega_header', 'omega_wrap_open', 7 );
+//add_action('omega_header', 'omega_wrap_close' );
 
 add_action('omega_before_main', 'omega_wrap_open', 7 );
 add_action('omega_after_main', 'omega_wrap_close' );
@@ -17,7 +17,7 @@ add_action('omega_after_main', 'omega_wrap_close' );
 add_action('omega_before_primary_menu', 'omega_wrap_open', 7 );
 add_action('omega_after_primary_menu', 'omega_wrap_close' );
 
-add_action('omega_footer', 'omega_wrap_open', 7 );
-add_action('omega_footer', 'omega_wrap_close' );
+//add_action('omega_footer', 'omega_wrap_open', 7 );
+//add_action('omega_footer', 'omega_wrap_close' );
 
 ?>
--- omega/partials/entry-title.php
+++ omega/partials/entry-title.php
@@ -3,7 +3,7 @@ if ( is_home() || is_archive() || is_search() ) {
 ?>
 	<h1 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 <?php		
-} else {
+} else if (get_the_title() != 'Index') {
 ?>
 	<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
 <?php
